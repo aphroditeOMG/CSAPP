5192a05226a2b89c26d7a1c9bd3db0093f70b7fd
APh <aphroditedpissu@gmail.com>
2017-09-24 22:39:38 +0800

    a little change

--- Unit2/2.64.c
+++ Unit2/2.64.c
@@ -3,7 +3,7 @@
 /* Return 1 when any odd bit of x equals 1; 0 otherwise.
    Assume w = 32*/
     int any_odd_one(unsigned x){
-    return !!(x & 0x01010101);
+    return !!(x & 0x55555555);
     }
 
 int main(){
