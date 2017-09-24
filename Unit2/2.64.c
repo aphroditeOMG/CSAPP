3f2410162192664f2afc69e40bc8e9f74463791d
aphroditeOMG <aphroditedpissu@gmail.com>
2017-09-24 14:45:46 +0800

    changed the test

--- Unit2/2.64.c
+++ Unit2/2.64.c
@@ -9,7 +9,7 @@
 int main(){
     printf("%d\n", any_odd_one(0x01010101));
     printf("%d\n", any_odd_one(0x11111111));
-    printf("%d\n", any_odd_one(0x10101010));
+    printf("%d\n", any_odd_one(0x22222222));
     printf("%d\n", any_odd_one(0x00000000));
     return 0;
 }
